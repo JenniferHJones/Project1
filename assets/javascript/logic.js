99da7071ed6d20e70dc423c1e80428cbd24bf9d9
Lindca <45475418+Lindca@users.noreply.github.com>
2019-02-05 22:42:21 -0800

    added some psuedo code for Page 1

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -34,49 +34,109 @@ $(document).ready(function () {
     });
     // collect email address check if it is real with above api
     // password needed and email add to database or make sure is in database
+    //--------------------------PAGE 1 Login or Sign Up----------------------------//
+    var email;
+    var password;
+    var newName;
+    var newEmail;
+    var newPassword;
+    $("#login").on("click", function () {
+        email = $("#loginEmail").val().firm();
+        password = $("#loginPass").val().firm();
 
-    //----------------------------------------------------------------//
-    var formKeyword = encodeURI(keyword);
-    var queryURL = "https://www.food2fork.com/api/search?key=7698efbc320ad7432bc2688700f3f72f&q=" + formKeyword;
+        // query the firebase database and check if this is a listed login
+        // email and password are in database
+        if (response.obj.email = email && response.obj.password= password){
+        // then take them to page 2 where they can choose what to search by
 
-    //api key https://www.food2fork.com/api/search?key=YOUR_API_KEY&q=chicken%20breast&page=2 
+        }
 
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).then(function (response) {
-        console.log(JSON.parse(response));
+    })
+
+// on click of sign up bottom show input fields email name and password
+// then add these to the database and have an on click button of "Complete Sign Up"
+// "Complete Sign Up" button brings you back to login
+$("#signUp").on("click", function () {
+    newEmail = $("#signupEmail").val().firm();
+    newPassword = $("#signupPass").val().firm();
+    newName = $("#signupName").val().firm();
+
+    // add this login info to the database and 
+    database.ref(loginCred).set({
+        email: newEmail,
+        password: newPassword,
+        name: newName,
     });
-    var queryURL = "https://www.food2fork.com/api/get?key=7698efbc320ad7432bc2688700f3f72f&rId=" + recipeId;
-
-    //api key https://www.food2fork.com/api/search?key=YOUR_API_KEY&q=chicken%20breast&page=2 
-
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).then(function (response) {
-        console.log(JSON.parse(response));
-    });
-
-    // $("#chosenRecipe").on("click", function (){
-    //     var recipeId=response.recipe.id
+})
 
 
-    // })
 
-    // Zomato ajax call is limited to 20 results; need to use offset so user can see more results on button click
-    var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=499&entity_type=city&cuisines=indian&sort=rating";
-    // var offsetNumZomato = 0;
+//-----------------------------Page 3 Recipe Select-----------------------------------//
 
-    $.ajax({
-        url: queryURL,
-        headers: {
-            "user-key": "28f92b35782adcf900a661e108616e3a",
-            "Accept": "application/json"
-        },
-        method: "GET",
-    }).then(function (response) {
-        console.log(response);
-    });
+var formKeyword = encodeURI(keyword);
+var queryURL = "https://www.food2fork.com/api/search?key=7698efbc320ad7432bc2688700f3f72f&q=" + formKeyword;
+
+//api key https://www.food2fork.com/api/search?key=YOUR_API_KEY&q=chicken%20breast&page=2 
+
+$.ajax({
+    url: queryURL,
+    method: "GET",
+}).then(function (response) {
+    console.log(JSON.parse(response));
+});
+
+var newRevcDiv = $("<div class= 'recipeBox'>")
+
+
+$("#checkoutRecipe").on("click", function () {
+
+
+})
+
+
+//-------------------------------Page 3 Look at recipe-----------------------//
+
+var queryURL = "https://www.food2fork.com/api/get?key=7698efbc320ad7432bc2688700f3f72f&rId=" + recipeId;
+
+//api key https://www.food2fork.com/api/search?key=YOUR_API_KEY&q=chicken%20breast&page=2 
+
+$.ajax({
+    url: queryURL,
+    method: "GET",
+}).then(function (response) {
+    console.log(JSON.parse(response));
+});
+
+// $("#chosenRecipe").on("click", function (){
+//     var recipeId=response.recipe.id
+
+
+// })
+//-------------------------------------Page 3 Choose Restaurant-------------//
+// Zomato ajax call is limited to 20 results; need to use offset so user can see more results on button click
+var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=499&entity_type=city&cuisines=indian&sort=rating";
+// var offsetNumZomato = 0;
+
+$.ajax({
+    url: queryURL,
+    headers: {
+        "user-key": "28f92b35782adcf900a661e108616e3a",
+        "Accept": "application/json"
+    },
+    method: "GET",
+}).then(function (response) {
+    console.log(response);
+});
+
+var newResDiv = $("<div class= 'restaurantBox'>");
+
+
+$("#checkoutMenu").on("click", function () {
+
+
+})
+
+
+
 
 })
