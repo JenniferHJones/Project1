f8c8f97a151aebd1c6406c01e497555630aee628
MSI\jeff <hatcher28@me.com>
2019-02-06 19:41:37 -0800

    fix

--- assets/javascript/results.js
+++ assets/javascript/results.js
@@ -1,12 +1,11 @@
 console.log("from results.js")
 
-var img = '';
+var img = 'dd';
 var name = "KRU";
 var address = "1255 Folsom Blvd.";
 var rating = 4.2;
 var menuUrl = 'Menu';
 
-img.src = '.assets/images/kru.png';
 
 // A $( document ).ready() block.
 $( document ).ready(function() {
@@ -15,11 +14,11 @@ $( document ).ready(function() {
 
 // puts data to dom 
 var newRow = $('<tr>');
-newRow.append($("<td><img>" + img.src + "</img></td>"));
+newRow.append($("<td>" + img + "</td>"));
 newRow.append($("<td>" + name + "</td>"));
 newRow.append($("<td>" + address + "</td>"));
 newRow.append($("<td>" + rating+ "</td>"));
-newRow.append($('<td><button>' + menuUrl + "</buttom></td>"));
+newRow.append($("<td><button>" + menuUrl + "</buttom></td>"));
 
 
 
