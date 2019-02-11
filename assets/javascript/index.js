2d7fe47fe921d9294c7e6451107846ab26b9149c
JJ <jenjones@ucdavis.edu>
2019-02-10 18:53:28 -0800

    firebase authentication, various css changes, index.html changes

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,6 +1,58 @@
 $(document).ready(function () {
 
-    //firebase setup
+    loadPage();
+
+    function loadPage() {
+        $("#registerDiv").hide();
+        $("#loginDiv").hide();
+        $("#choicesDiv").hide();
+    }
+
+    // Function to run when user clicks Register button in navbar
+    $("#navRegister").click(displayRegister);
+
+    function displayRegister() {
+        console.log("register clicked");
+        $("#registerDiv").show();
+        $("#loginDiv").hide();
+        $("#choicesDiv").hide();
+    }
+
+    // Function to run when user clicks Login button in navbar
+    $("#navLogin").click(displayLogin);
+
+    function displayLogin() {
+        console.log("login clicked");
+        $("#loginDiv").show();
+        $("#registerDiv").hide();
+        $("#choicesDiv").hide();
+    }
+
+    // Function to run when user clicks submit button after registering or logging in
+    $(".submit").click(displayChoices);
+
+    function displayChoices() {
+        $("#choicesDiv").show();
+        $("#loginDiv").hide();
+        $("#registerDiv").hide();
+    }
+
+    // Function to run when user clicks Logout button in navbar
+    $("#navLogout").click(userLogOut);
+
+    function userLogOut() {
+        console.log("logout clicked");
+        firebase.auth().signOut();
+        $("#choicesDiv").hide();
+        $("#loginDiv").hide();
+        $("#registerDiv").hide();
+    };
+
+    $("#showLogin").click(displayLogin);
+
+    $("#showRegister").click(displayRegister);
+
+    // Firebase setup
     var config = {
         apiKey: "AIzaSyAXBHM2-Rfs25DdM3vmaGK3CDbjGlZMkQM",
         authDomain: "cravings-7c8a8.firebaseapp.com",
@@ -9,96 +61,129 @@ $(document).ready(function () {
         storageBucket: "cravings-7c8a8.appspot.com",
         messagingSenderId: "928328970018"
     };
+
     firebase.initializeApp(config);
-    
+
     var database = firebase.database();
 
-    // var clickCounter = 0;
+    // // Function to run based on if user is logged in or not
+    // firebase.auth().onAuthStateChanged(function (user) {
+    //     if (user) {
+    //         // User is signed in.
 
-    // $("#test").on("click", function () {
-    //     clickCounter++;
+    //     } else {
+    //         // No user is signed in.
 
-    //     database.ref().set({
-    //         clickCount: clickCounter
-    //     });
+    //     }
     // });
 
-    // Add login event
-    $("#signInButton").on("click", function (event) {
+    // Function to register a new user
+    $(".register form").on("submit", function (event) {
         event.preventDefault();
 
-        // Sign in
-        var email = $("#email").val();
-        var password = $("#password").val();
+        var email = $(".register .email").val();
+        var password = $(".register .password").val();
 
-        firebase
-            .auth()
-            .signInWithEmailAndPassword(email, password)
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(function (user) {
+                console.log(user);
+            })
+            .catch(function (err) {
+                console.log(err);
+            })
+    });
 
-            .catch(function (error) {
-                var errorCode = error.code;
-                var errorMessage = error.message;
+    // Function to login existing user
+    $(".login form").on("submit", function (event) {
+        event.preventDefault();
+
+        var email = $(".login .email").val();
+        var password = $(".login .password").val();
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(function (user) {
+                console.log(user);
+            })
+            .catch(function (err) {
+                console.log(err);
             });
     });
 
-    // Add signup event
-    $("#signUpButton").on("click", e => {
-        e.preventDefault();
-        var email = $("#signup-email").val();
-        var password = $("#signup-password").val();
-        // Get  FB authentication
-        auth = firebase.auth();
-        // Sign In
-        promise = auth.createUserWithEmailAndPassword(email, password);
-        promise.catch(e => console.log(e.message));
-    });
+    // // BONUS - Function to allow user to record their favorites
+    // firebase.auth().onAuthStateChanged(function (user) {
 
-    // to validate email addresses entered, this API works!
-    var email;
-    var queryURL = "http://apilayer.net/api/check?access_key=079da406e2f7aaa1714f04c3adcc3efc&email=" + email + "&smtp=1&format=1"
+    //     // turns event listener off
+    //     $(".star").off();
 
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).then(function (response) {
-        console.log(response);
-    });
-    // collect email address check if it is real with above api
-    // password needed and email add to database or make sure is in database
-    //--------------------------PAGE 1 Login or Sign Up----------------------------//
-    var email;
-    var password;
-    var newName;
-    var newEmail;
-    var newPassword;
-    $("#login").on("click", function () {
-        email = $("#loginEmail").val().firm();
-        password = $("#loginPass").val().firm();
+    //     if (user) {
 
-        // query the firebase database and check if this is a listed login
-        // email and password are in database
+    //         $(".star").on("submit", function(event) {
+    //             event.preventDefault();
 
-        //     if (response.obj.email = email && response.obj.password= password){
-        //     // then take them to page 2 where they can choose what to search by
-        // }
+    //             var favorite = $(".star").click;
 
-    })
+    //             firebase.database().ref("/users/" + user.uid).child("/favorites").push(star);
+    //         })
+    //     } else {
+    //         $(".star").on("submit", function(event) {
+    //             console.log("Please log in");
+    //         })
+    //     }
+    // });
 
-    // on click of sign up bottom show input fields email name and password
-    // then add these to the database and have an on click button of "Complete Sign Up"
-    // "Complete Sign Up" button brings you back to login
-    $("#signUp").on("click", function () {
-        newEmail = $("#signupEmail").val().firm();
-        newPassword = $("#signupPass").val().firm();
-        newName = $("#signupName").val().firm();
 
-        // add this login info to the database and 
-        database.ref(loginCred).set({
-            email: newEmail,
-            password: newPassword,
-            name: newName,
-        });
+    ///////////////////////////////////////////////
+    //
+    // work with Lindsay to incorporate code below
+    //
+    ///////////////////////////////////////////////
 
-    });
+    // // to validate email addresses entered, this API works!
+    // var email;
+    // var queryURL = "http://apilayer.net/api/check?access_key=079da406e2f7aaa1714f04c3adcc3efc&email=" + email + "&smtp=1&format=1"
+
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET",
+    // }).then(function (response) {
+    //     console.log(response);
+    // });
+    // // collect email address check if it is real with above api
+    // // password needed and email add to database or make sure is in database
+    // //--------------------------PAGE 1 Login or Sign Up----------------------------//
+    // var email;
+    // var password;
+    // var newName;
+    // var newEmail;
+    // var newPassword;
+    // $("#login").on("click", function () {
+    //     email = $("#loginEmail").val().firm();
+    //     password = $("#loginPass").val().firm();
+
+    //     // query the firebase database and check if this is a listed login
+    //     // email and password are in database
+
+    //     //     if (response.obj.email = email && response.obj.password= password){
+    //     //     // then take them to page 2 where they can choose what to search by
+    //     // }
+
+    // })
+
+    // // on click of sign up bottom show input fields email name and password
+    // // then add these to the database and have an on click button of "Complete Sign Up"
+    // // "Complete Sign Up" button brings you back to login
+    // $("#signUp").on("click", function () {
+    //     newEmail = $("#signupEmail").val().firm();
+    //     newPassword = $("#signupPass").val().firm();
+    //     newName = $("#signupName").val().firm();
+
+    //     // add this login info to the database and 
+    //     database.ref(loginCred).set({
+    //         email: newEmail,
+    //         password: newPassword,
+    //         name: newName,
+    //     });
+
+    // });
 
 })
